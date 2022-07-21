dcb59aec790743710982a09da14492a5fe427217
miroslavdurin <miro990@hotmail.com>
2022-07-21 11:25:55 +0200

    bug fixes

--- src/components/AllJobs/AllJobs.jsx
+++ src/components/AllJobs/AllJobs.jsx
@@ -6,14 +6,13 @@ function AllJobs({allJobs}) {
     const [filters, setFilters] = useState([]);
     const [jobs, setJobs] = useState([]);
 
-    /* role, level, languages, tools */
-
-
     useEffect(()=>{
-        setJobs([...allJobs]);
-    }, [])
+        
+        if(filters.length === 0) {
+            setJobs([...allJobs]);
+            return;
+        }
 
-    useEffect(()=>{
         /* Declaring and using new variables is necessary because useState doesn't always update synchronously and there
         was a bug when some filters were removed */
 
@@ -43,22 +42,30 @@ function AllJobs({allJobs}) {
         const removedFilter = e.target.closest(".btn-remove-filter").dataset.filter;
 
         const newFilters = filters.filter(f=> f.toLowerCase() !== removedFilter.toLowerCase());
-
         
         setFilters([...newFilters]);
         setJobs([...allJobs]);
     }
 
+    function handleClearAllFilters() {
+        setFilters([]);
+    }
+
     return (
-        <div className='container' key={filters}>
+        <div className={`container ${filters.length > 0 && 'translate'}`} key={filters}>
             {filters.length > 0 &&
-                <div className="filters-container" onClick={handleRemoveFilters}>   
-                    { filters.map(filter=> 
-                        <button key={filter + " filter"} data-filter={filter} className='btn-remove-filter'>
-                            <span>{filter}</span>
-                            <svg className="remove-icon" id="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32"><path fill-rule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/></svg>
-                        </button> 
-                    )}           
+                <div className="filters-container" onClick={handleRemoveFilters}> 
+                    <div className="filters-buttons">
+                        { filters.map(filter=> 
+                            <button key={filter + " filter"} data-filter={filter} className='btn-remove-filter'>
+                                <span>{filter}</span>
+                                <svg className="remove-icon" id="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32"><path fill-rule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/></svg>
+                            </button> 
+                        )}   
+                    </div>  
+                    <div className="filters-btn-clear">
+                        <button onClick={handleClearAllFilters} className="btn-clear-filters">Clear</button>
+                    </div>
                 </div>
             }
             {                
